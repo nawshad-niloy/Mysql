0c4d54f666c52b34daae83348ba332aeed48c98f
nawshad-niloy <151645651+nawshad-niloy@users.noreply.github.com>
2025-02-15 21:51:21 +0600

    First DBMS Lab Done

---
+++ SQL Lab 1/Director_query.sql
@@ -0,0 +1,2 @@
+use movie_info;
+select Director_Name,No_Of_Awards from Director where Director_Name like '_a%';
--- SQL Lab 1/movie_query.sql
+++ SQL Lab 1/movie_query.sql
@@ -9,3 +9,12 @@ select Movie_Name,IMDB_Rating,Director_Name from movie join Director on Director
 WHERE Genre = 'Thriller';
 
 select Movie_Name,Genre from movie where Movie_Name like 'A%';
+
+select Movie_Name from movie join Director on Director_ID = Person_ID where Movie_Name like "%Byomkesh%";
+
+select Movie_Name,Year from movie where length(movie_name) = 2;
+
+select Movie_Name,Genre from movie where length(movie_name) >= 5;
+
+select Movie_Name,IMDB_Rating,Director_Name from movie join Director on Director_ID = Person_ID
+where length(movie_name) <= 10;
